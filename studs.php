3b49df1ca62f372df3e0f932633f79200a4f0148
Borghesi Guilhem <borghesi@unistra.fr>
2008-12-03 10:20:12 +0000

    correction de plusieurs bugs d'affichage de IE dans studs.php
    svn path=/; revision=52

--- studs.php
+++ studs.php
@@ -486,7 +486,7 @@ else {
 	
 	echo '<br><br>'."\n";
 	echo '<p class=affichageexport>'."\n";
-	echo 'R&eacute;cup&eacute;ration des donn&eacute;es : Tableau (.CSV) <input type="image" name="exportcsv" value="Export en CSV" src="images/csv.ico" alt="Export CSV">  ';
+	echo 'R&eacute;cup&eacute;ration des donn&eacute;es : Tableau (.CSV) <input type="image" name="exportcsv" value="Export en CSV" src="images/csv.png" alt="Export CSV">  ';
  		if (($dsondage->format=="D"||$dsondage->format=="D+")&&$compteursujet=="1"&&$meilleurecolonne){
   			echo ' &nbsp;Agenda (.ICS) :<input type="image" name="exportics" value="Export en iCal" src="images/ical.png" alt="Export iCal">';
   			$_SESSION["meilleursujet"]=$meilleursujetexport;
