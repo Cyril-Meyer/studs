1b3245c3107a4f90964219b16e913fd4a633632d
Borghesi Guilhem <borghesi@unistra.fr>
2009-02-01 14:30:57 +0000

    bugs corrigés par bug et effacement complet du contenu de logs_studs.txt
    svn path=/; revision=78

--- install.sql
+++ install.sql
@@ -2,38 +2,8 @@
 -- PostgreSQL database dump
 --
 
-SET client_encoding = 'LATIN9';
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
-SET search_path = public, pg_catalog;
-
-ALTER TABLE ONLY public.user_studs DROP CONSTRAINT user_studs_pkey;
-ALTER TABLE ONLY public.sondage DROP CONSTRAINT sondage_pkey;
-DROP TABLE public.user_studs;
-DROP TABLE public.sujet_studs;
-DROP TABLE public.sondage;
-DROP SCHEMA public;
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: postgres
---
-
-CREATE SCHEMA public;
-
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON SCHEMA public IS 'Standard public schema';
-
-
-SET default_tablespace = '';
-
-SET default_with_oids = true;
-
---
--- Name: sondage; Type: TABLE; Schema: public; Owner: borghesi; Tablespace: 
+-- Name: sondage; Type: TABLE;
 --
 
 CREATE TABLE sondage (
@@ -50,7 +20,7 @@ CREATE TABLE sondage (
 
 
 --
--- Name: sujet_studs; Type: TABLE; Schema: public; Owner: borghesi; Tablespace: 
+-- Name: sujet_studs; Type: TABLE;
 --
 
 CREATE TABLE sujet_studs (
@@ -60,7 +30,7 @@ CREATE TABLE sujet_studs (
 
 
 --
--- Name: user_studs; Type: TABLE; Schema: public; Owner: borghesi; Tablespace: 
+-- Name: user_studs; Type: TABLE;
 --
 
 CREATE TABLE user_studs (
@@ -72,7 +42,44 @@ CREATE TABLE user_studs (
 
 
 --
--- Name: sondage_pkey; Type: CONSTRAINT; Schema: public; Owner: borghesi; Tablespace: 
+-- Data for Name: sondage; Type: TABLE DATA;
+--
+
+COPY sondage (id_sondage, commentaires, mail_admin, nom_admin, titre, id_sondage_admin, date_fin, format, mailsonde) FROM stdin;
+aqg259dth55iuhwm	Repas de Noel du service	Stephanie@saillard.com	Stephanie	Repas de Noel	aqg259dth55iuhwmy9d8jlwk	1327100361	D+	
+\.
+
+
+--
+-- Data for Name: sujet_studs; Type: TABLE DATA;
+--
+
+COPY sujet_studs (id_sondage, sujet) FROM stdin;
+aqg259dth55iuhwm	1225839600@12h,1225839600@19h,1226012400@12h,1226012400@19h,1226876400@12h,1226876400@19h,1227049200@12h,1227049200@19h,1227826800@12h,1227826800@19h
+\.
+
+
+--
+-- Data for Name: user_studs; Type: TABLE DATA;
+--
+
+COPY user_studs (nom, id_sondage, reponses, id_users) FROM stdin;
+marcel	aqg259dth55iuhwm	0110111101	933
+paul	aqg259dth55iuhwm	1011010111	935
+sophie	aqg259dth55iuhwm	1110110000	945
+barack	aqg259dth55iuhwm	0110000	948
+takashi	aqg259dth55iuhwm	0000110100	951
+albert	aqg259dth55iuhwm	1010110	975
+alfred	aqg259dth55iuhwm	0110010	1135
+marcs	aqg259dth55iuhwm	0100001010	1143
+laure	aqg259dth55iuhwm	0011000	1347
+benda	aqg259dth55iuhwm	1101101100	1667
+Albert	aqg259dth55iuhwm	1111110011	1668
+\.
+
+
+--
+-- Name: sondage_pkey; Type: CONSTRAINT;
 --
 
 ALTER TABLE ONLY sondage
@@ -80,24 +87,9 @@ ALTER TABLE ONLY sondage
 
 
 --
--- Name: user_studs_pkey; Type: CONSTRAINT; Schema: public; Owner: borghesi; Tablespace: 
+-- Name: user_studs_pkey; Type: CONSTRAINT;
 --
 
 ALTER TABLE ONLY user_studs
     ADD CONSTRAINT user_studs_pkey PRIMARY KEY (id_users);
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
 
