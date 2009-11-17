b2b22477cbd976ada9592cbbd17b0807222c4644
Borghesi Guilhem <borghesi@unistra.fr>
2009-11-17 10:39:00 +0000

    depoussierage du fichier sources.php
    svn path=/; revision=151

--- sources/sources.php
+++ sources/sources.php
@@ -38,13 +38,64 @@
 //==========================================================================
 
 include '../bandeaux.php';
+include '../variables.php';
 session_start();
+
+if ($_POST["uk"]){
+        $_SESSION["langue"]="EN";
+}
+if ($_POST["germany"]){
+        $_SESSION["langue"]="DE";
+}
+if ($_POST["france"]){
+        $_SESSION["langue"]="FR";
+}
+if ($_POST["espagne"]){
+        $_SESSION["langue"]="ES";
+}
+if ($_SESSION["langue"]==""){
+        $_SESSION["langue"]=getenv('LANGUE');
+}
+
 
 //Choix de la langue
 if ($_SESSION["langue"]=="FR"){ include '../lang/fr.inc';}
 if ($_SESSION["langue"]=="EN"){ include '../lang/en.inc';}
 if ($_SESSION["langue"]=="DE"){ include '../lang/de.inc';}
 if ($_SESSION["langue"]=="ES"){ include '../lang/es.inc';}
+
+//action des boutons sous bandeaux
+if ($_POST["intranet"]){
+	header("Location:../admin/index.php");
+exit();
+}
+
+if ($_POST["contact"]){
+        header("Location:../contacts.php");
+        exit();
+}
+
+if ($_POST["sources"]){
+        header("Location:sources.php");
+        exit();
+}
+
+if ($_POST["exemple"]){
+        header("Location:../studs.php?sondage=aqg259dth55iuhwm");
+        exit();
+}
+
+if ($_POST["apropos"]){
+        header("Location:../apropos.php");
+        exit();
+}
+//bouton annuler
+if ($_POST["annuler"]||$_POST["annuler_x"]){
+        header("Location:../index.php");
+        exit();
+}
+
+
 
 echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">'."\n";
 echo '<html>'."\n";
@@ -54,15 +105,20 @@ echo '<title>Sources STUdS</title>'."\n";
 echo '<link rel="stylesheet" type="text/css" href="../style.css">'."\n";
 echo '</head>'."\n";
 echo '<body>'."\n";
+
+echo '<form name=formulaire action="sources.php" method="POST">'."\n";
+
 sous_logo();
 bandeau_tete();
 bandeau_titre();
+sous_bandeau();
 echo '<div class=corpscentre>'."\n";
 print "<H2>$tt_sources_lien</H2><br><br>"."\n";
 print "$tt_choix_page_erreur_retour<a href=\"../index.php\"> STUdS</A>. "."\n";
 echo '<br><br><br>'."\n";
 echo '</div>'."\n";
 
+echo '</form>'."\n";
 
 // Affichage du bandeau de pied
 bandeau_pied();
